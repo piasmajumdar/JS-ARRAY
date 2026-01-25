3baae0dd5a6fa03ec6a525443340191c3cb5558c
piasmajumdar <piasmajumdar@gmail.com>
2026-01-25 22:54:02 +0530

    added practice task 2

---
+++ practice_task_2.js
@@ -0,0 +1,19 @@
+/* . Add or remove elements
+Declare an array of 3 tourist destinations
+Add a new tourist destination to your tourist array
+Add two more to your array
+Remove the last tourist destination you have added
+display the final array as output */
+
+let dest = ['Nepal', 'Bhutan', 'Thailand'];
+console.log(dest);
+
+dest.push('Bangkok');
+console.log(dest);
+
+dest.push('Canada');
+dest.push('America');
+console.log(dest);
+
+dest.pop();
+console.log(dest);
