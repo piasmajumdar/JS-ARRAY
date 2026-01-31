bb3e923982cbeb5be79b70768c55f61378896a68
piasmajumdar <piasmajumdar@gmail.com>
2026-02-01 03:20:26 +0530

    practice task 4 completed

---
+++ practice_task_4.js
@@ -0,0 +1,9 @@
+const place = ['Vijayawada', 'Chennai', 'Andhra', 'Bengaluru'];
+const age = [10, 20, 30];
+const BMI = 50;
+const name = 'Pias';
+
+console.log(Array.isArray(place));
+console.log(Array.isArray(age));
+console.log(Array.isArray(BMI));
+console.log(Array.isArray(name));
