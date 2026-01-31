1d3b2d76bafa420bed9d4a8d2e679ac797eaeeba
piasmajumdar <piasmajumdar@gmail.com>
2026-02-01 03:23:58 +0530

    practice task 5 completed -- concat()

---
+++ practice_task_5.js
@@ -0,0 +1,10 @@
+const place = ['Vijayawada', 'Chennai', 'Andhra', 'Bengaluru'];
+const age = [10, 20, 30];
+
+let placeAge = place.concat(age);
+
+console.log(place);
+console.log(age);
+console.log(placeAge);
+
+console.log(place.join('|'));
